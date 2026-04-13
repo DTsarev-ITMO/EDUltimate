137c9d740f0a984e295b9735df1c339bf6760e9c
dvtsarev <dmitriy_93@mail.ru>
2026-04-13 15:38:29 +0300

    Написан RESTfull API для работы с базой данных пользователей: аутентификация и роли

--- app/food/router.py
+++ app/food/router.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter, Depends
 from app.food.dao import FoodDAO
-from app.food.schemas import ResponseFoodGet, ResponseFoodAdd, ResponseFoodUpdate #,ResponseFoodUpdName,ResponseFoodUpdProtein, ResponseFoodUpdFats, ResponseFoodUpdCarbs
+from app.food.schemas import ResponseFoodGet, ResponseFoodAdd, ResponseFoodUpdate
 from app.food.rb import RBFood
+from app.users.dependencies import get_current_admin_user
+from app.users.models import User
 
 router = APIRouter(prefix='/food', tags=['Работа с продуктами'])
 
@@ -17,7 +19,7 @@ async def get_student_by_filter(request_body: RBFood = Depends()) -> ResponseFoo
     return rez
 
 @router.post("/add/")
-async def add_food(food: ResponseFoodAdd) -> dict:
+async def add_food(food: ResponseFoodAdd, myself: User = Depends(get_current_admin_user)) -> dict:
     check = await FoodDAO.add(**food.model_dump())
     if check:
         return {"message": "Продукт успешно добавлен!", "продукт": food}
@@ -25,7 +27,7 @@ async def add_food(food: ResponseFoodAdd) -> dict:
         return {"message": "Ошибка при добавлении продукта!"}
 
 @router.put("/update/")
-async def update(food: ResponseFoodUpdate) -> dict:
+async def update(food: ResponseFoodUpdate, myself: User = Depends(get_current_admin_user)) -> dict:
     check = await FoodDAO.update(filter_by={'name': food.filter_name},
                                  **food.model_dump(exclude={'filter_name'}, exclude_none=True))
     if check:
@@ -34,7 +36,7 @@ async def update(food: ResponseFoodUpdate) -> dict:
         return {"message": "Ошибка при обновлении записи!"}
 
 @router.delete("/delete/{food_name}")
-async def delete_food(food_name: str) -> dict:
+async def delete_food(food_name: str, myself: User = Depends(get_current_admin_user)) -> dict:
     check = await FoodDAO.delete(name=food_name)
     if check:
         return {"message": f"Продукт {food_name} удален!"}
--- app/users/router.py
+++ app/users/router.py
@@ -1,17 +1,81 @@
-from fastapi import APIRouter, Depends
+from fastapi import Response
 from app.users.dao import UserDAO
-from app.users.schemas import ResponseUser
-from app.users.rb import RBUser
+from app.users.schemas import ResponseUserRegister, ResponseUserAuth, ResponseUserMakeAdmin, ResponseUserUpdate
+from fastapi import APIRouter, HTTPException, status, Depends
+from app.users.auth import get_password_hash, authenticate_user, create_access_token
+from app.users.dependencies import get_current_user, get_current_admin_user, get_current_super_admin_user
+from app.users.models import User
 
-router = APIRouter(prefix='/users', tags=['Работа с пользователями'])
+router = APIRouter(prefix='/auth', tags=['Работа с пользователями'])
 
-@router.get("/", summary="Получить всех пользователей", response_model=list[ResponseUser])
-async def get_all_users():
+@router.post("/register/")
+async def register_user(user_data: ResponseUserRegister) -> dict:
+    user = await UserDAO.find_one_or_none(email=user_data.email)
+    if user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail='Пользователь уже существует'
+        )
+    user_dict = user_data.model_dump()
+    user_dict['password'] = get_password_hash(user_data.password)
+    await UserDAO.add(**user_dict)
+    return {'message': 'Вы успешно зарегистрированы!'}
+
+@router.post("/login/")
+async def auth_user(response: Response, user_data: ResponseUserAuth):
+    check = await authenticate_user(email=user_data.email, password=user_data.password)
+    if check is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Неверная почта или пароль')
+    access_token = create_access_token({"sub": str(check.id)})
+    response.set_cookie(key="user_access_token", value=access_token, httponly=True)
+    return {'access_token': access_token, 'refresh_token': None}
+
+@router.get("/me/")
+async def get_me(user_data: User = Depends(get_current_user)):
+    return user_data
+
+@router.post("/logout/")
+async def logout_user(response: Response):
+    response.delete_cookie(key="user_access_token")
+    return {'message': 'Пользователь вышел из системы'}
+
+@router.get("/all_users/")
+async def get_all_users(user_data: User = Depends(get_current_admin_user)):
     return await UserDAO.find_all()
 
-@router.get("/by_filter", summary="Получить одного пользователя по фильтру")
-async def get_user_by_filter(request_body: RBUser = Depends()) -> ResponseUser | dict:
-    rez = await UserDAO.find_one_or_none(**request_body.to_dict())
-    if rez is None:
-        return {'message': f'Пользователь с указанными параметрами не найден!'}
-    return rez
+@router.post("/make_admin/")
+async def make_admin(user_to_update_id: ResponseUserMakeAdmin, myself: User = Depends(get_current_super_admin_user)):
+    check = await UserDAO.update(filter_by={'id': user_to_update_id}, is_admin=True)
+    new_admin = await UserDAO.find_one_or_none(id=user_to_update_id)
+    if check:
+        return {"message": "Запись успешно обновлена!", "Новый администратор": new_admin}
+    else:
+        return {"message": "Ошибка при добавлении администратора!"}
+
+@router.put("/update/")
+async def update(user_data = ResponseUserUpdate, current_user: User = Depends(get_current_user)) -> dict:
+    check = await UserDAO.update(filter_by={'id': current_user.id},
+                                 **user_data.model_dump(exclude_none=True))
+    if check:
+        return {"message": "Запись успешно обновлена!", "данные пользователя": user_data}
+    else:
+        return {"message": "Ошибка при обновлении записи!"}
+
+
+
+@router.delete("/delete_me/")
+async def delete_me(myself: User = Depends(get_current_user)):
+    check = await UserDAO.delete(id=myself.id)
+    if check:
+        return {"message": f"Ваш аккаунт успешно удален!"}
+    else:
+        return {"message": "Ошибка при удалении аккаунта"}
+
+@router.delete("/delete/")
+async def delete_user(user_to_delete_id: int, myself: User = Depends(get_current_admin_user)):
+    check = await UserDAO.delete(id=user_to_delete_id)
+    if check:
+        return {"message": f"Аккаунт успешно удален!"}
+    else:
+        return {"message": "Ошибка при удалении аккаунта"}
