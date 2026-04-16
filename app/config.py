8974bc15f60320b04f8587cba8ee2938d1f480e6
dvtsarev <dmitriy_93@mail.ru>
2026-04-16 15:54:57 +0300

    API и DB в контейнерах и общаются

--- app/config.py
+++ app/config.py
@@ -5,19 +5,19 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DB_USER: str
     DB_PASSWORD: str
-    DB_HOST: str
-    DB_PORT: int
     DB_NAME: str
+    DB_INTERNAL_HOST: str
+    DB_INTERNAL_PORT: int
     SECRET_KEY: str
     ALGORITHM: str
 
     model_config = SettingsConfigDict(
-        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "../.env")
+        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), ".env")
     )
 
     def get_db_url(self):
         return (f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@"
-                f"{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}")
+                f"{self.DB_INTERNAL_HOST}:{self.DB_INTERNAL_PORT}/{self.DB_NAME}")
 
     def get_auth_data(self):
         return {"secret_key": self.SECRET_KEY, "algorithm": self.ALGORITHM}
