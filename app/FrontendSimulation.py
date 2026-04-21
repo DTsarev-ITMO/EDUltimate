eacd652952d63e228427503aaf9997d61cfd2fde
dvtsarev <dmitriy_93@mail.ru>
2026-04-21 15:57:57 +0300

    Все стабильно запускается

--- app/FrontendSimulation.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-import aiohttp
-
-from tests.users.httpx_commands import register_user, log_in, get_me, update_me, delete_me
-
-if __name__ == '__main__':
-    response = asyncio.run(delete_me(email='delme@mail.ru', password='password'))
-    print(response, {'message': 'Ваш аккаунт успешно удален!'})
