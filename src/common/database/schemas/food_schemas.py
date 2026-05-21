1bacd54802de0886403593c6ebcad3e6e1cac73a
dvtsarev <dmitriy_93@mail.ru>
2026-05-21 12:32:17 +0300

    [fix]: поправлены схемы еды

--- src/common/database/schemas/food_schemas.py
+++ src/common/database/schemas/food_schemas.py
@@ -7,14 +7,3 @@ class ResponseFood(BaseModel):
     fats: float = Field(..., ge=0, le=100, description="Содержание жиров на 100 грамм продукта, от 0 до 100 грамм")
     carbs: float = Field(..., ge=0, le=100, description="Содержание углеводов на 100 грамм продукта, от 0 до 100 грамм")
     calories: float = Field(0, ge=0, description="Содержание калорий на 100 грамм продукта, не меньше 0")
-
-# class ResponseFoodFind(BaseModel):
-#     name: str = Field(..., min_length=1, max_length=50, description="Название продукта, от 1 до 50 символов")
-
-# class ResponseFoodUpdate(BaseModel):
-#     filter_name: str = Field(..., description="Название продукта для поиска")
-#     name: str = Field(None, min_length=1, max_length=50, description="Новое название продукта, от 1 до 50 символов")
-#     protein: float = Field(None, ge=0, le=100, description="Содержание белка на 100 грамм продукта, от 0 до 100 грамм")
-#     fats: float = Field(None, ge=0, le=100, description="Содержание жиров на 100 грамм продукта, от 0 до 100 грамм")
-#     carbs: float = Field(None, ge=0, le=100, description="Содержание углеводов на 100 грамм продукта, от 0 до 100 грамм")
-#     calories: float = Field(None, ge=0, description="Содержание калорий на 100 грамм продукта, не меньше 0")
